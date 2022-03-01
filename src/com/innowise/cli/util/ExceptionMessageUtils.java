35a7e90cf0a49d96290921cb383791a8cd28b42e
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-01 18:40:28 +0300

    update ExceptionMessageUtils

--- src/com/innowise/cli/util/ExceptionMessageUtils.java
+++ src/com/innowise/cli/util/ExceptionMessageUtils.java
@@ -5,6 +5,8 @@ public final class ExceptionMessageUtils {
     public static final String DRIVER_ERROR = "DriverManager can not load driver";
     public static final String PROPERTIES_ERROR = "Data base can not load properties!";
     public static final String CONNECTION_ERROR = "Driver can not build connection";
+    public static final String DAO_EXCEPTION_MESSAGE = "Dao layer exception message";
+    public static final String SERVICE_EXCEPTION_MESSAGE = "Service layer exception message";
 
     private ExceptionMessageUtils() {
         throw new UnsupportedOperationException();
