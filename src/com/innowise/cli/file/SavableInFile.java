7b69de2c64beca2d4cd76cb2c27c1f2868ef6eb8
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-01 18:51:46 +0300

    add file package

--- src/com/innowise/cli/file/SavableInFile.java
+++ src/com/innowise/cli/file/SavableInFile.java
@@ -1,9 +1,10 @@
 package com.innowise.cli.file;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Path;
 
 public interface SavableInFile<T extends Serializable> {
 
-    void saveInFile(T entity, Path path);
+    void saveInFile(T entity, Path path) throws IOException;
 }
