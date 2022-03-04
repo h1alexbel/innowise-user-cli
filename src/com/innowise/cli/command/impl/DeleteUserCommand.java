44dfd0fb05251a3b09e67b5152af6d6a5c81e5b4
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-04 16:24:14 +0300

    add DeleteUserCommand

---
+++ src/com/innowise/cli/command/impl/DeleteUserCommand.java
@@ -0,0 +1,30 @@
+package com.innowise.cli.command.impl;
+
+import com.innowise.cli.command.Command;
+import com.innowise.cli.exception.CommandException;
+import com.innowise.cli.exception.ServiceException;
+import com.innowise.cli.service.impl.UserServiceImpl;
+import com.innowise.cli.util.ConsoleMessagesUtils;
+import com.innowise.cli.util.ExceptionMessageUtils;
+
+import java.util.Scanner;
+
+public class DeleteUserCommand implements Command {
+
+    private final UserServiceImpl userService = UserServiceImpl.getInstance();
+
+    @Override
+    public void execute(Scanner scanner) throws CommandException {
+        long id = Long.parseLong(scanner.next());
+        try {
+            boolean isDeleted = userService.deleteUserById(id);
+            if (isDeleted) {
+                System.out.println(ConsoleMessagesUtils.USER_SUCCESSFULLY_DELETED);
+            } else {
+                System.out.println(ConsoleMessagesUtils.USER_IS_NOT_EXISTS);
+            }
+        } catch (ServiceException e) {
+            throw new CommandException(ExceptionMessageUtils.COMMAND_EXCEPTION_MESSAGE, e);
+        }
+    }
+}
