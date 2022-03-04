4a183d1aa701055e928606fe5195bc7b555893d2
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-04 16:37:01 +0300

    update OperationUtils

--- src/com/innowise/cli/util/OperationUtils.java
+++ src/com/innowise/cli/util/OperationUtils.java
@@ -9,8 +9,12 @@ public final class OperationUtils {
     public static final String FIND_BY_ID = "4.Find user by id";
     public static final String UPDATE = "5.Update user";
     public static final String DELETE = "6.Delete user";
-    public static final String ENTER = "Enter your choice";
-    public static final String CREDENTIALS_FAILED_MESSAGE = "Credentials are invalid, try again!";
+    public static final String ADD_ROLE_TO_USER = "7.Add role to user";
+    public static final String ADD_PHONE_NUMBER_TO_USER = "8.Add phone to user";
+    public static final String SHOW_USER_PHONE_INFO = "9. Show all user phones";
+    public static final String SHOW_USER_ROLE_INFO = "10. Show all user roles";
+    public static final String ENTER = "Enter your choice [just type 1,2 or etc]";
+    public static final String THERE_IS_NO_SUCH_COMMAND_MESSAGE = "Is not command, try again!";
 
     private OperationUtils() {
         throw new UnsupportedOperationException();
