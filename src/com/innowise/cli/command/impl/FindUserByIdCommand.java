f3584b25b71540761fa3cee0a993898030789bba
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-03-05 13:09:43 +0300

    update FindUserByIdCommand

--- src/com/innowise/cli/command/impl/FindUserByIdCommand.java
+++ src/com/innowise/cli/command/impl/FindUserByIdCommand.java
@@ -3,10 +3,13 @@ package com.innowise.cli.command.impl;
 import com.innowise.cli.command.Command;
 import com.innowise.cli.exception.CommandException;
 import com.innowise.cli.exception.ServiceException;
+import com.innowise.cli.model.PhoneNumber;
+import com.innowise.cli.model.Role;
 import com.innowise.cli.model.User;
 import com.innowise.cli.service.impl.UserServiceImpl;
 import com.innowise.cli.util.ExceptionMessageUtils;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
@@ -21,7 +24,11 @@ public class FindUserByIdCommand implements Command {
             Optional<User> maybeUser = userService.getById(id);
             if (maybeUser.isPresent()) {
                 User user = maybeUser.get();
+                List<PhoneNumber> phoneNumbers = userService.getUserPhoneNumbers(user.getId());
+                List<Role> roles = userService.getUserRoles(user.getId());
                 System.out.println(user);
+                System.out.println(roles);
+                System.out.println(phoneNumbers);
             }
         } catch (ServiceException e) {
             throw new CommandException(ExceptionMessageUtils.COMMAND_EXCEPTION_MESSAGE, e);
